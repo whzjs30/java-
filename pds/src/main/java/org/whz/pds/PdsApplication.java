0464a3fee76cdaa2b717b6b5c4b7be6ca271dd08
whz <249121208@qq.com>
2020-04-03 14:39:03 +0800

    4-3 @whz

---
+++ pds/src/main/java/org/whz/pds/PdsApplication.java
@@ -0,0 +1,13 @@
+package org.whz.pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsApplication.class, args);
+	}
+
+}
